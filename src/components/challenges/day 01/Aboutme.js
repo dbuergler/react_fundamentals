ee306fae97a8a58bd8da457a10b6e5f3ab39a4ed
Daniel Buergler <daniel.jbuergler@gmail.com>
2021-01-26 21:21:19 -0500

    Created functional components

---
+++ src/components/challenges/day 01/Aboutme.js
@@ -0,0 +1,23 @@
+const Aboutme = () => {
+    return (
+        <>
+        <h1>Daniel Buergler</h1>
+    <p>I am a student at Eleven Fifty Academy and Program Manager at National FFA Organization</p>
+
+    <ul>
+        <li>I have a bachelors degree in history and political science.</li>
+        <li>My fastest half marathon is 1:14:41.</li>
+        <li>I took a class trip to Europe </li> 
+    </ul>
+        </>
+    )
+};
+
+
+
+
+
+
+export default Aboutme;
+
+
